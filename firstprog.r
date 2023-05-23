c56ba36f49941e7ba678c1a39f3ca7bb213ab385
Tomke Splettstößer <113780717+tomkespl21@users.noreply.github.com>
2023-05-23 17:09:02 +0200

    Add files via upload

--- firstprog.r
+++ firstprog.r
@@ -8,20 +8,38 @@
 rm(list=ls())     # clear workspace
 
 library(tidyverse)
+library(lubridate)
+library(zoo)
 
-
-#Data <- read.csv("C:/work/R/Data/firstprog_data.csv", header =TRUE,sep=";", dec="." , check.names=TRUE)
+# Reitz option 
+data2 <- read.csv("firstprog_data.csv", header =TRUE,sep=";", dec="." , check.names=TRUE)
 # I prefer to set working direction before s.t. path not needed 
 
-# read_csv2 because its semicolon separated
-data <- read_csv2("firstprog_data.csv")
+
+data <- read_delim("firstprog_data.csv",
+                    delim = ";",
+                    col_names = T,
+                    locale=locale(decimal_mark = "."))
+
 
 view(data)
+str(data)
+
+
+
+# seeing first colum isnt a date variable
+data <- 
+  data %>%
+  rename(date = ...1) %>%
+  mutate(date = ymd(paste(date, "-01", sep="")))
+  
+  
+  
 
 #print(data[1:10,])
 # print function not really necessary 
 data[1:10,]
-
+data2[1:10,]
 
 # How long is the data set(time series dimension)?
 # T <- dim(Data)[1]
@@ -31,18 +49,34 @@ data[1:10,]
 # indexing to get number of rows 
 # R starts counting at 1 not 0 ! 
 TS <- dim(data)[1]
+TS
 
 # Variable definitions
+# start at 2 since we gonna use lag variables
 
-price <- data[2:TS,5]
-prod <- data[2:TS,2]
-wage <- data[2:TS,4]
-unemp <- data[2:TS,8]
+data <- 
+  data %>% 
+  mutate(price = data[2:TS,5],
+         prod  = data[2:TS,2],
+         wage  = data[2:TS,4],
+         unemp = data[2:TS,8],
+         pricelag = lag(price,1),
+         prodlag  = lag(prod,1),
+         wagelag  = lag(wage,1),
+         unemplag = lag(unemp,1),
+         ) 
+  
 
-# Reitz lagging
-#pricelag <- Data[1:(TS-1),5]
-#prodlag <- Data[1:(TS-1),2]
-#wagelag <- Data[1:(TS-1),4]
+# Reitz variable transformations
+#price <- data[2:TS,5]
+#prod <- data[2:TS,2]
+#wage <- data[2:TS,4]
+#unemp <- data[2:TS,8]
+
+
+#pricelag_R <- data[1:(TS-1),5]
+#prodlag_R <- data[1:(TS-1),2]
+#wagelag_R <- data[1:(TS-1),4]
 
 
 # lagging with dplyr 
@@ -51,6 +85,8 @@ prodlag <- lag(prod,1)
 wagelag <- lag(wage,1)
 unemplag <- lag(unemp,1)
 
+# test if lag variavles are the same
+pricelag == pricelag_R
 
 
 
@@ -58,50 +94,54 @@ unemplag <- lag(unemp,1)
 
 
 # Variable transformations
-
 pricedouble <- price * 2
 lnprice <- log(price)
 lnpricelag <- log(pricelag)
 
 
 infl <- 100*(lnprice - lnpricelag)
-w <- 100*(log(wage) - log(wagelag))
-mpl <- 100*(log(prod) - log(prodlag))
-
-# also check the function diff()
+w <- 100*(log(wage) - log(wagelag)) # wage changes
+mpl <- as.vector(100*(log(prod) - log(prodlag))) # prod changes
 
 
-TS <- length(infl)  # Adjust length of the time series
+
+TS <-  dim(infl)[1] # Adjust length of the time series
 
 # Graphing Data
-
-graphdata <- cbind(mpl,w)
+ 
+graphdata <- cbind(mpl,w) # combine mpl with w
 
 plot.ts(graphdata, plot.type = "multiple")
 # indexing because mpl and w are data frames
-plot(mpl[,1],w[,1])
+ggplot(data,aes(x=w[[1]],y=mpl[[1]]))+
+  geom_point()+
+  xlab("mpl")+
+  ylab("w")+
+  ggtitle("Scatterplot")
+
+
+  
 
 
 # Doing simple regressions
-
-X <- cbind(infl[4:(T-1)],infl[3:(T-2)],infl[2:(T-3)],infl[1:(T-4)],mpl[5:T])
-y <- w[5:T]
+X <- cbind(infl[4:(TS-1)],infl[3:(TS-2)],infl[2:(TS-3)],infl[1:(TS-4)],mpl[5:TS])
+y <- w[5:TS]
 
 wageeq1 <- lm(y ~ X)
 summary(wageeq1)
 
 
 ############################
-X <- cbind(infl[4:(T-1)],infl[3:(T-2)],infl[2:(T-3)],infl[1:(T-4)],mpl[5:T],unemp[5:T])
-y <- w[5:T]
+X <- cbind(infl[4:(TS-1)],infl[3:(TS-2)],infl[2:(TS-3)],infl[1:(TS-4)],mpl[5:TS],unemp[5:T])
+y <- w[5:TS]
 colnames(X) <- c("Infl_-1", "Infl_-2", "Infl_-3", "Infl_-4", "MPL", "Unemp")
 
 wageeq2 <- lm(y ~ X)
 summary(wageeq2)
 
 ############################
-X <- cbind(infl[4:(T-1)],infl[3:(T-2)],infl[2:(T-3)],infl[1:(T-4)],mpl[5:T],unemp[5:T])
-y <- infl[5:T]
+X <- cbind(infl[4:(TS-1)],infl[3:(TS-2)],infl[2:(TS-3)],infl[1:(TS-4)],mpl[5:TS],unemp[5:TS])
+y <- infl[5:TS]
 colnames(X) <- c("Infl_-1", "Infl_-2", "Infl_-3", "Infl_-4", "MPL", "Unemp")
 
 Philleq <- lm(y ~ X)
@@ -128,9 +168,9 @@ summary(Philleqfirst)
 #Work with regression output
 
 U <- Philleq$residuals
-inflhat <- infl[5:T] - U
+inflhat <- infl[5:TS] - U
 
-graphdata <- cbind(infl[5:T],inflhat)
+graphdata <- cbind(infl[5:TS],inflhat)
 plot.ts(graphdata, plot.type = "single", col = c("blue","red"))
 
 ########################################################
@@ -138,10 +178,10 @@ plot.ts(graphdata, plot.type = "single", col = c("blue","red"))
 #Durbin Watson Statistic
 U <- Philleq$residuals
 #U <- wageeq2$residuals
-T <- length(U)
+TS <- length(U)
 
-ut <- U[2:T]
-ut_1 <- U[1:(T-1)]
+ut <- U[2:TS]
+ut_1 <- U[1:(TS-1)]
 
 DW <- (t(ut - ut_1)%*%(ut - ut_1))/(t(ut)%*%ut)
 print(DW)
